442dc26ab1340b0f420cf79cec6eec5216f58bf7
Sho Hirose <sho.hirose@gmail.com>
2023-01-01 18:21:49 +0900

    Format

--- run_all.ps1
+++ run_all.ps1
@@ -1,5 +1,4 @@
-$simulators = "first_order_upwind", "lax_wendroff",  "beam_warming", "fromm", "tvd_minmod", "tvd_superbee", "tvd_van_leer", "tvd_van_albada"
-foreach ($simulator in $simulators)
-{
+$simulators = "first_order_upwind", "lax_wendroff", "beam_warming", "fromm", "tvd_minmod", "tvd_superbee", "tvd_van_leer", "tvd_van_albada"
+foreach ($simulator in $simulators) {
     & ./build/$simulator
 }
