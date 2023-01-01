a3058db107752dc3359b13f6634c1e3d28dee210
Sho Hirose <sho.hirose@gmail.com>
2023-01-01 23:17:53 +0900

    Add comments to ExplicitEulerScheme

--- include/cfd/time_integration_schemes.hpp
+++ include/cfd/time_integration_schemes.hpp
@@ -7,14 +7,38 @@
 
 namespace cfd {
 
+/**
+ * @brief Time integration with explicit Euler scheme
+ *
+ * The explicit Euler scheme for the linear scalar advection equation can be
+ * expressed by
+ * @f[
+ * u_j^{n+1} = u_j^n - \frac{\Delta t}{\Delta x} \cdot
+ *    (\hat{f}_{j+1/2} - \hat{f}_{j-1/2})
+ * @f]
+ * where @f$ \hat{f} @f$ is numerical flux at cell boundaries.
+ */
 class ExplicitEulerScheme {
  public:
+  /**
+   * @brief Construct a new Explicit Euler Scheme object
+   *
+   * @param params Problem parameters
+   */
   ExplicitEulerScheme(const ProblemParameters& params)
       : dx_{params.dx},
         dt_{params.dt},
         n_boundary_cells_{params.n_boundary_cells},
         n_domain_cells_{params.n_domain_cells} {}
 
+  /**
+   * @brief Construct a new Explicit Euler Scheme object
+   *
+   * @param dx Grid length
+   * @param dt Time step length
+   * @param n_boundary_cells Number of boundary cells
+   * @param n_domain_cells Number of domain cells
+   */
   ExplicitEulerScheme(double dx, double dt, int n_boundary_cells,
                       int n_domain_cells)
       : dx_{dx},
@@ -22,6 +46,14 @@ class ExplicitEulerScheme {
         n_boundary_cells_{n_boundary_cells},
         n_domain_cells_{n_domain_cells} {}
 
+  /**
+   * @brief Update @f$ u @f$
+   *
+   * @tparam Derived1
+   * @tparam Derived2
+   * @param u Variable to solve
+   * @param f Numerical flux
+   */
   template <typename Derived1, typename Derived2>
   void update(Eigen::MatrixBase<Derived1>& u,
               const Eigen::MatrixBase<Derived2>& f) const noexcept {
