00a2560b941adaa49c480b4b7add013d337572d1
Sho Hirose <sho.hirose@gmail.com>
2023-01-28 21:22:41 +0900

    Fix: check if simulators exist

--- run_all.ps1
+++ run_all.ps1
@@ -1,4 +1,7 @@
 $simulators = "first_order_upwind", "lax_wendroff", "beam_warming", "fromm", "tvd_minmod", "tvd_superbee", "tvd_van_leer", "tvd_van_albada"
 foreach ($simulator in $simulators) {
+    if (-not (Test-Path ".\build\${simulator}.exe")) {
+        throw ".\build\${simulator}.exe not found!"
+    }
     & ./build/$simulator
 }
